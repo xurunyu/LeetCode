10721fb95cddd7bd8c8df6dde29c27a3c81b6605
runyu.xry <runyu.xry@antfin.com>
2020-05-28 19:10:44 +0800

    add two questions

---
+++ src/leetcode/editor/cn/LinkedListCycleLcci.java
@@ -0,0 +1,40 @@
+  //给定一个有环链表，实现一个算法返回环路的开头节点。 有环链表的定义：在链表中某个节点的next元素指向在它前面出现过的节点，则表明该链表存在环路。 示例 1
+//： 输入：head = [3,2,0,-4], pos = 1 输出：tail connects to node index 1 解释：链表中有一个环，其尾部连
+//接到第二个节点。 示例 2： 输入：head = [1,2], pos = 0 输出：tail connects to node index 0 解释：链表中有
+//一个环，其尾部连接到第一个节点。 示例 3： 输入：head = [1], pos = -1 输出：no cycle 解释：链表中没有环。 进阶： 你是否可以不
+//用额外空间解决此题？ Related Topics 链表
+
+  
+  package leetcode.editor.cn;
+  public class LinkedListCycleLcci{
+      public static void main(String[] args) {
+           Solution solution = new LinkedListCycleLcci().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+}
