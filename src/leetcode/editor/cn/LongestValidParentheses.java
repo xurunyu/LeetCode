d9a6723f585ad7a02e02a142fea0c56c40faadea
runyu.xry <runyu.xry@antfin.com>
2021-04-07 19:20:52 +0800

    on for longestvalidparenthese

--- src/leetcode/editor/cn/LongestValidParentheses.java
+++ src/leetcode/editor/cn/LongestValidParentheses.java
@@ -45,22 +45,24 @@ package leetcode.editor.cn;
 public class LongestValidParentheses{
     public static void main(String[] args) {
         Solution solution = new LongestValidParentheses().new Solution();
+        System.out.println(solution.longestValidParentheses("(()())"));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestValidParentheses(String s) {
-        int[] dp = new int[s.length() + 1];
+        int result = 0;
+        int[] dp = new int[s.length()];
         for (int i = 1; i < s.length(); i++) {
-            char si = s.charAt(i);
-
-            if (si == ')') {
-
-                char s2 = s.charAt(i-1);
-                if (s2 == '(') {
-                    dp[]
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i-2] : 0) + 2;
+                } else if (s.charAt(i - 1) == ')' && i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i-1] + 2 + (i - dp[i - 1] >= 2? dp[i - dp[i - 1] -2] : 0) ;
                 }
             }
+            result = Math.max(result, dp[i]);
         }
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
